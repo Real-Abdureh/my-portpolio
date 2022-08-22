316865435f4fbeba9751aa84e188320e8763b026
Real-Abdureh <abdouliabbas75@gmail.com>
2022-08-22 05:13:47 -0700

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-console.log('its working')
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
 
 let themeDots = document.getElementsByClassName('theme-dot')
 
@@ -9,4 +17,24 @@ for (var i=0; themeDots.length > i; i++){
 		console.log('Option clicked:', mode)
 		setTheme(mode)
 	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
 }
